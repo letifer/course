adb92f3b26ac5c4b5e0e50a492bd77c6196781fe
letifer <sanguispugnus@gmail.com>
2014-09-04 22:48:45 +0600

    fixed - problem

--- PythonWeek3/ExPyth3-1.py
+++ PythonWeek3/ExPyth3-1.py
@@ -3,19 +3,22 @@ def is_number(x):
     result = False
     commacntr = 0 #количество встреченных в строке запятых
     for i in range(len(x)): #простая посимвольная проверка
-        if x[0] == "-": #для выявления
-            result = True #отрицательных чисел
-        elif x[i] == "." or x[i] == ",":
-            commacntr += 1
-            if commacntr > 1:
+        if x == "-0":
+            break
+        elif x[0].isdigit() or x[0] == "-":
+            if x[i] == "," or x[i] == ".":
+                commacntr += 1
+                if commacntr > 1:
+                    break
+                else:
+                    continue
+            elif x[i].isdigit():
+                result = True
+                continue
+            else:
                 result = False
                 break
-            else:
-                continue
-        elif x[i].isdigit():
-            result = True
         else:
-            result = False
             break
     return result
 
